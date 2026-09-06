0b569e4a199073f37e80478ac0cdf8b97b5d852c
Ulises Viña <ulisesv@ciencias.unam.mx>
2026-09-05 23:48:10 -0600

    chores: Agregué un archivo a .gitignore

--- perfil/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('confirmar/', views.confirmar_django, name='confirmar_django'),
-]
